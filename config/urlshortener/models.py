c05f6c6b15ea2695e7683d2424e574ceb4861bdc
Zainab Lawal <zainablaw1012@gmail.com>
2021-10-19 00:04:19 +0300

    Created the Shortner model

--- config/urlshortener/models.py
+++ config/urlshortener/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .utils import create_shortened_url
 
 # Create your models here.
 class Shortener(models.Model):
@@ -12,3 +13,9 @@ class Shortener(models.Model):
 
     def __str__(self):
         return f'{self.long_url} to {self.short_url}'
+
+    def save(self, *args, **kwargs):
+        if not self.short_url:
+            self.short_url = create_shortened_url(self)
+
+        super().save(*args, **kwargs)
